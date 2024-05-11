306e3556008fc2e0467732b515fdcd8dd959bb59
Kat Brandt <joyfulkat11@gmail.com>
2024-05-10 19:32:44 -0600

    Add book show endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   namespace :api do
     namespace :v1 do 
-      resources :books, only: [:index]
+      resources :books, only: [:index, :show]
     end
   end
 end
--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -32,4 +32,34 @@ describe "Books API" do
       end
     end
   end
+
+  describe "GET /books/:id" do 
+    it "shows one book" do 
+      book = create(:book)
+      
+      get "/api/v1/books/#{book.id}"
+
+      expect(response).to be_successful
+      
+      book_response = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(book_response).to have_key(:id)
+      expect(book_response[:id]).to eq book.id
+
+      expect(book_response).to have_key(:title)
+      expect(book_response[:title]).to eq book.title
+
+      expect(book_response).to have_key(:author)
+      expect(book_response[:author]).to eq book.author
+
+      expect(book_response).to have_key(:summary)
+      expect(book_response[:summary]).to eq book.summary
+
+      expect(book_response).to have_key(:genre)
+      expect(book_response[:genre]).to eq book.genre
+
+      expect(book_response).to have_key(:number_sold)
+      expect(book_response[:number_sold]).to eq book.number_sold
+    end
+  end
 end
